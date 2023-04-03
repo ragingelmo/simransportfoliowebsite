01e8acb1957296be2239df1186ef2eb5a568d819
Simran Singh <simran27@bu.edu>
2023-04-03 18:45:49 -0400

    Importing Nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import Experience from './pages/Experience'
-import Contact from './pages/Contact'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Experience from './pages/Experience';
+import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
