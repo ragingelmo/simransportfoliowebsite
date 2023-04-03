0b584c265f0f409a36d47320a742fc924b38f471
Simran Singh <simran27@bu.edu>
2023-04-03 18:42:45 -0400

    Basic NavBar structure

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,15 @@ import {Link} from 'react-router-dom';
 
 function Navbar() {
     return (
-        <div className = "navigationbar">
+        <div className ="navigationbar">
+            <div className ="button1">
+            <button> </button>
             </div>
+            <div className ="linkto">
+                <Link to ="/"> Home </Link>
+                <Link to ="/"> Home </Link>
+            </div>
+        </div>
     );
 }
 
